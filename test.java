e69ebbe64d53f54ae318194d2624907f236f4580
achen285-ops <achen285@student.fuhsd.org>
2026-04-13 20:41:50 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-public class test {
+public class Test {
     public static void main(String[] args) {
         Board board = new Board(Board.BoardType.DEFAULT);
         board.initializePieces(Board.BoardType.DEFAULT);
