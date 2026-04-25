60ee789bf58f4e2dd9190dee72320dd7ec00f4bd
luuMax <141470810+luuMax@users.noreply.github.com>
2026-04-24 17:43:01 -0700

    added castling logic
    - castling logic
    - King class now overrides Piece's move() method to move both King and Rook during castling.

--- King.java
+++ King.java
@@ -8,9 +8,6 @@ public class King extends Piece
         super(color, row, col);
     }
 
-
-
-    // just for king so theres no infinite loop in isInCheck()
     public boolean canMoveTo(int fromRow, int fromCol, int toRow, int toCol, Board board) {
         if (toRow >= 0 && toRow < 8 && toCol >= 0 && toCol < 8 && Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1) {
             if (board.getPieceAt(toRow, toCol) != null && board.getPieceAt(toRow, toCol).getColor() != this.getColor()) {
@@ -20,7 +17,61 @@ public class King extends Piece
                 return true;
             }
         }
+        // castling
+        else if (toRow == fromRow && isFirstMove && Math.abs(toCol-fromCol) == 2 && !isInCheck(getColor(), board)) {
+            int dir = (int) Math.signum(toCol - fromCol);
+            if (isSafeMove(fromRow, fromCol, toRow, fromCol + dir, board) && isSafeMove(fromRow, fromCol, toRow, fromCol + 2 * dir, board)) {
+                Piece piece;
+                if (dir == 1) {
+                    if (isPathClear(fromRow, fromCol, toRow, 7, board)) {
+                        piece = board.getPieceAt(toRow, 7);
+                    }
+                    else {
+                        return false;
+                    }
+                    
+                }
+                else {
+                    if (isPathClear(fromRow, fromCol, toRow, 0, board)) {
+                        piece = board.getPieceAt(toRow, 0);
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                if (piece instanceof Rook && piece.isFirstMove) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+
+    // override move method specifically for castling
+    @Override
+    public void move(int fromRow, int fromCol, int toRow, int toCol, Board board)
+    {
+        if (isLegalMove(fromRow, fromCol, toRow, toCol, board)) {
+            board.getBoard()[toRow][toCol] = this;
+            board.getBoard()[fromRow][fromCol] = null;
+            this.setRow(toRow);
+            this.setCol(toCol);
+        }
+        int dir = (int) Math.signum(toCol - fromCol);
+        Rook rook;
+        if (dir == 1) {
+            rook = (Rook) board.getPieceAt(toRow, 7);
+            board.getBoard()[toRow][fromCol + dir] = rook;
+            board.getBoard()[toRow][7] = null;
+            
+        }
+        else {
+            rook = (Rook) board.getPieceAt(toRow, 0);
+            board.getBoard()[toRow][fromCol + dir] = rook;
+            board.getBoard()[toRow][0] = null;
+        }
+        isFirstMove = false;
+
     }
 
     // Used for check, checkmate, pin logic
