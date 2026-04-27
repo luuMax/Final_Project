1b42f724737fd12f4d026cdfd1622413bb64f4f1
Nel <86257024+Neel-1709@users.noreply.github.com>
2026-04-27 11:25:09 -0700

    Update BoardUI.java

--- BoardUI.java
+++ BoardUI.java
@@ -9,13 +9,27 @@ public class BoardUI extends JFrame{
         mainPanel.setLayout(new BorderLayout());
 
         setTitle("Chat");
-        setSize(600,600);
+        setSize(800,800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(8,8));
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                JPanel square = new JPanel();
+                if ((i + j) % 2 == 0) {
+                    square.setBackground(Color.WHITE);
+                } else {
+                    square.setBackground(Color.GRAY);
+                }
+                add(square);
+            }
+        }
         setVisible(true);
     }
+
     public static void main(String[] args) {
-        BoardUI bo = new BoardUI();
-        bo.initialize();
+        BoardUI board = new BoardUI();
+        board.initialize();
     }
 
 }
