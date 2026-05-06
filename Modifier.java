18a6790da59bd93f8460e9366ec67aabb7053acc
luuMax <141470810+luuMax@users.noreply.github.com>
2026-05-05 22:54:16 -0700

    updated Modifier.java

--- Modifier.java
+++ Modifier.java
@@ -1,7 +1,20 @@
-public abstract class Modifier {
+public class Modifier {
+    public enum Type {
+        // BRAINSTORM 
+        PAWNS_ONLY, KNIGHTS_ONLY, BISHOPS_ONLY, ROOKS_ONLY, QUEENS_ONLY, KINGS_ONLY, EXPLODING_PIECE
+    }
     int turnsRemaining;
+    private Type type;
 
-    public Modifier(int turns) {
+    public Modifier(int turns, Type type) {
         turnsRemaining = turns;
+    }
+
+    public Type getType() {
+        return type;
+    }
+    
+    public int getTurnsRemaining() {
+        return turnsRemaining;
     }
 }
