db55d4966aa88cff78ad62109a43836420a7775a
achen285-ops <achen285@student.fuhsd.org>
2026-05-08 20:09:14 -0700

    Update GameRunner.java

--- GameRunner.java
+++ GameRunner.java
@@ -12,37 +12,7 @@ public class GameRunner //basically gamerunner, boardui, and modifiers are all t
     //testing, currently boardui has no references to game
     public void start() {
         game = new Game();
-        Board board = new Board();
         boardUI = new BoardUI(800, 800, 30, game); 
-
-
-                //just random tests rn
-        System.out.println(game.getCurrentTurn());
-        Board imagineNamingYourSonBoard2 = new Board();
-        for (Piece[] pieceRow : imagineNamingYourSonBoard2.getBoard()) {
-            for (Piece piece : pieceRow) {
-                if (piece != null) {
-                    char file = (char) ('a' + piece.getCol());
-                    int rank = 8 - piece.getRow();
-                    String currentNotation = "" + file + rank;
-                    String n;
-
-                    switch (piece) {
-                        case King _ -> {n= "King";}
-                        case Queen _ -> {n = "Queen";}
-                        case Rook _ -> {n = "Rook";}
-                        case Knight _ -> {n = "Knight";}
-                        case Pawn _ -> {n = "Pawn";}
-                        case Bishop _ -> {n = "Bishop";}
-                        default -> {n = "Nothing";}
-                    }
-
-                    
-
-                    System.out.println(n + " at " + currentNotation);
-                }
-            }
-        }
     }
 
 }
