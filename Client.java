6be1cbf820ba35e4c04960b50744af1d4428f8b7
achen285-ops <achen285@student.fuhsd.org>
2026-05-18 11:21:04 -0700

    some testing changes
    not some much

--- Client.java
+++ Client.java
@@ -1,9 +1,18 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        connect("localhost"); // change to server's IP when on different machines
+        //currently testing connections; will fix/connect to mainmenuui later with gamelink approaproately. 
+        System.out.println("localhost");
+        System.out.println("172.18.231.34");
+        System.out.println("10.18.81.146");
+        System.out.println("input gamelink: ");
+        Scanner scan = new Scanner(System.in);
+        String hoster = scan.nextLine();
+
+        connect(hoster); // change to server's IP when on different machines
     }
 
     public static void connect(String hostIP) {
