0fda66157b61b077d9beb023a8930772fe4b182e
luuMax <141470810+luuMax@users.noreply.github.com>
2026-05-21 13:04:16 -0700

    balanced portal
    you cannot place portals on occupied squares

--- BoardUI.java
+++ BoardUI.java
@@ -406,19 +406,33 @@ public class BoardUI extends JFrame
         }
 
         if (placingPortal1) {
+            System.out.println("Place portal 1");
+            Piece target = boardgrid.getPieceAt(i, j);
+            if (target != null) {
+                System.out.println("You cannot place a portal on an occupied square");
+                return; // can't place on an occupied square
+            }
             game.setPortal1Pos(i, j);
             placingPortal1 = false;
             placingPortal2 = true;
             redrawBoard();
+            System.out.println("Portal 1 placed at " + i + " " + j);
             return;
         }
 
         if (placingPortal2) {
+            System.out.println("Place portal 2");
+            Piece target = boardgrid.getPieceAt(i, j);
+            if (target != null) {
+                System.out.println("You cannot place a portal on an occupied square");
+                return; // can't place on an occupied square
+            }
             game.setPortal2Pos(i, j);
             game.setPortalsActive(true);
             game.setModifierOfferedThisCycle(true);
             placingPortal2 = false;
             redrawBoard();
+            System.out.println("Portal 2 placed at " + i + " " + j);
             return;
         }
 
