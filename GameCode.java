3e1c6d3efa2b8290bf8f92ef8b16adf0fcc2322f
achen285-ops <achen285@student.fuhsd.org>
2026-05-21 12:49:57 -0700

    delete fluff

--- GameCode.java
+++ GameCode.java
@@ -31,8 +31,4 @@ class GameCode {
             return "127.0.0.1";
         }
     }
-
-    public static void main( String[] args) {
-        System.out.println(GameCode.encode(GameCode.getIpAddress()));
-    }
 }
