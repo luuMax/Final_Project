a055cf1c11d0a7edd5177391516f7521ea4ec924
Nel <neelsparimi@gmail.com>
2026-05-27 19:23:55 -0700

    Delete StartTest.java

--- StartTest.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class StartTest //2 program tester
-{
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainMenuUI(800, 800));
-    }
-}
